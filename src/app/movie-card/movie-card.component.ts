3ec62e4dbff8658502a2a055998e3f204e418a3e
JNicholas54 <102398723+JNicholas54@users.noreply.github.com>
2023-10-13 15:21:57 -0700

    fixing movie-card component

--- src/app/movie-card/movie-card.component.ts
+++ src/app/movie-card/movie-card.component.ts
@@ -3,6 +3,7 @@ import { FetchApiDataService } from '../fetch-api-data.service';
 import { MatDialog } from '@angular/material/dialog';
 import { MatSnackBar } from '@angular/material/snack-bar';
 import { MovieDetailDialogComponent } from '../movie-detail-dialog/movie-detail-dialog.component';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-movie-card',
@@ -14,10 +15,18 @@ export class MovieCardComponent implements OnInit {
   constructor(
     public fetchApiData: FetchApiDataService,
     public dialog: MatDialog,
-    public snackBar: MatSnackBar
+    public snackBar: MatSnackBar,
+    public router: Router
   ) {}
 
   ngOnInit(): void {
+    const user = localStorage.getItem('user');
+
+    if (!user) {
+      this.router.navigate(['welcome']);
+      return;
+    }
+
     this.getMovies();
   }
 
